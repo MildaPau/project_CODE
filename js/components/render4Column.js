c8569e4bf9c11d33192fea493a08f1241fa9a93f
MildaPau <76068034+MildaPau@users.noreply.github.com>
2021-02-09 21:38:07 +0200

    Render section three #4

---
+++ js/components/render4Column.js
@@ -0,0 +1,26 @@
+function render4Column(data) {
+    console.log(data);
+
+    return ` <div>
+                    <img src="./img/SectionThree_strategy_img.png" alt="">
+                    <h5>Strategy</h5>
+                    <p>We'll work with you to develop the right strategy</p>
+                </div>
+                <div>
+                    <img src="./img/SectionThree_automation_img.png" alt="">
+                    <h5>Automation</h5>
+                    <p>Work synchronization and automated services
+                    </p>
+                </div>
+                <div>
+                    <img src="./img/SectionThree_deploiment_img.png" alt="">
+                    <h5>Deployment</h5>
+                    <p>Dedicated servers to deploy and test applications</p>
+                </div>
+                <div>
+                    <img src="./img/SectionThree_education_img.png" alt="">
+                    <h5>Education</h5>
+                    <p>Stay informed on all our tools and technologies</p>
+                </div>`; 
+}
+
---
+++ js/data/dataFourColumn.js
@@ -0,0 +1,24 @@
+// DUOMENYS
+
+const dataFourColumn = [
+    {
+        img: "#",
+        h5: 'Strategy',
+        p: "We'll work with you to develop the right strategy",
+    },
+    {
+        img: "#",
+        h5: 'Automation',
+        p: "Work synchronization and automated services",
+    },
+    {
+        img:"#",
+        h5: 'Deployment',
+        p: "Dedicated servers to deploy and test applications",
+    },
+    {
+        img: "#",
+        h5: 'Education',
+        p: "Stay informed on all our tools and technologies",
+    },
+];
